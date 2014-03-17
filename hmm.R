b64c103470611d4ebedfe1b7410ba6891acddcda
gigacardoso <daniel.cardoso18@live.com>
2014-03-17 14:30:22 +0000

    training hmm on HMM.R

--- hmm.R
+++ hmm.R
@@ -1,12 +1,27 @@
  local({pkg <- select.list(sort(.packages(all.available = TRUE)),graphics=TRUE)
  if(nchar(pkg)) library(pkg, character.only=TRUE)})
 
-hmm = initHMM(c("A","B"),c("L","R"),transProbs=matrix(c(.25,.25,.25,.25),2),emissionProbs=matrix(c(.25,.25,.25,.25),2))
+ d <- read.csv(file="C:\\Users\\Daniel\\Documents\\GitHub\\HMMinR\\ALB.csv",head=TRUE,sep=",")
+
+ for (i in 1:nrow(d)) {
+	print(d[i,])
+	obs = d[i,]
+}
+# Initialise HMM
+hmm = initHMM(c("A","B"), 
+c("L","R"),
+transProbs=matrix(c(.8,.2,.2,.8),2),
+emissionProbs=matrix(c(.6,.4,.4,.6),2))
+
+#train HMM
+for each 
+baumWelch(hmm, observation, maxIterations=100, delta=1E-9, pseudoCount=0)
+
+
+emissionProbs=matrix(c(.6,.4,.4,.6),2))
 print(hmm)
-# Sequence of observation
-a = sample(c(rep("L",100),rep("R",300)))
-b = sample(c(rep("L",300),rep("R",100)))
-observation = c(a,b)
-# Viterbi-training
-vt = viterbiTraining(hmm,observation,10)
-print(vt$hmm)
+# Sequence of observations
+observations = c("L","L","L","R")
+# Calculate forward probablities
+logForwardProbabilities = forward(hmm,observations)
+print(exp(logForwardProbabilities))
