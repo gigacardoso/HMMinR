684165ebafde267ea35492f024812d75fe2c8df0
gigacardoso <daniel.cardoso18@live.com>
2014-03-17 14:45:47 +0000

    init hmm

--- hmm.R
+++ hmm.R
@@ -1,27 +1,32 @@
  local({pkg <- select.list(sort(.packages(all.available = TRUE)),graphics=TRUE)
  if(nchar(pkg)) library(pkg, character.only=TRUE)})
 
+#load data
  d <- read.csv(file="C:\\Users\\Daniel\\Documents\\GitHub\\HMMinR\\ALB.csv",head=TRUE,sep=",")
 
- for (i in 1:nrow(d)) {
-	print(d[i,])
-	obs = d[i,]
-}
 # Initialise HMM
 hmm = initHMM(c("A","B"), 
-c("L","R"),
-transProbs=matrix(c(.8,.2,.2,.8),2),
-emissionProbs=matrix(c(.6,.4,.4,.6),2))
+c("H","N","L","VL"),
+transProbs=matrix(c(.25,.25,.25,.25,
+.25,.25,.25,.25,
+.25,.25,.25,.25,
+.25,.25,.25,.25),4)
+emissionProbs=matrix(c(.25,.25,.25,.25,
+.25,.25,.25,.25,
+.25,.25,.25,.25,
+.25,.25,.25,.25),4))
 
 #train HMM
-for each 
-baumWelch(hmm, observation, maxIterations=100, delta=1E-9, pseudoCount=0)
-
-
-emissionProbs=matrix(c(.6,.4,.4,.6),2))
+ for (i in 1:nrow(d)) {
+	print(d[i,])
+	obs = d[1,2:ncol(d)]
+	baumWelch(hmm, obs, maxIterations=100, delta=1E-9, pseudoCount=0)
+}
 print(hmm)
+
 # Sequence of observations
 observations = c("L","L","L","R")
 # Calculate forward probablities
 logForwardProbabilities = forward(hmm,observations)
+
 print(exp(logForwardProbabilities))
